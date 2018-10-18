f883bf4555f6040883d3717d3be46a63b9a760fa
Howard <P95048490@capita.co.uk>
2018-10-18 14:27:57 +0100

    Simple HTTP server

--- Dockerfile
+++ Dockerfile
@@ -2,24 +2,11 @@
 # Go Builder Image
 FROM golang:1.11.1-alpine AS builder
 
-#RUN apk update && apk add curl git && apk add pkgconfig && apk add --no-cache librdkafka
-
-RUN apk add --update --no-cache alpine-sdk bash ca-certificates \
-      libressl \
-      tar \
-      git openssh openssl yajl-dev zlib-dev cyrus-sasl-dev openssl-dev build-base coreutils
-WORKDIR /root
-RUN git clone https://github.com/edenhill/librdkafka.git
-WORKDIR /root/librdkafka
-RUN /root/librdkafka/configure
-RUN make
-RUN make install
-RUN mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+#RUN apk update && apk add curl git
 
 RUN go get github.com/labstack/echo
 RUN go get github.com/labstack/echo/middleware
 RUN go get github.com/sirupsen/logrus
-RUN go get github.com/confluentinc/confluent-kafka-go/kafka
 
 # set build arguments: GitHub user and repository
 ARG GH_USER
--- test_http.go
+++ test_http.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/sirupsen/logrus"
@@ -30,42 +28,5 @@ func main() {
 }
 
 func goWithDocker(c echo.Context) error {
-	fmt.Println("Handler called :")
-
-	broker := "10.0.165.252:9092"
-	topic := "test-topic"
-
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": broker})
-
-	if err != nil {
-		fmt.Printf("Failed to create producer: %s\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Created Producer %v\n", p)
-
-	// Optional delivery channel, if not specified the Producer object's
-	// .Events channel is used.
-	deliveryChan := make(chan kafka.Event)
-
-	value := "Hello Go!"
-	err = p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-		Value:          []byte(value),
-		Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
-	}, deliveryChan)
-
-	e := <-deliveryChan
-	m := e.(*kafka.Message)
-
-	if m.TopicPartition.Error != nil {
-		fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
-	} else {
-		fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
-			*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
-	}
-
-	close(deliveryChan)
-
 	return c.JSON(http.StatusOK, "Go with Docker Container v2")
 }
